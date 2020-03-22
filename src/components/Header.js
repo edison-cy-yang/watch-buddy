75b31d8efafa0be64aa1d405f1b3a667cf9b9d79
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-22 15:12:47 -0700

    changed styling into scss file, added logout menuitem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -9,20 +8,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import './Header.scss';
 
 export default function MenuAppBar(props) {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -37,7 +25,7 @@ export default function MenuAppBar(props) {
   return (
       <AppBar position="static" className="header">
         <Toolbar>
-          <Typography variant="h4" className={classes.title}>
+          <Typography variant="h4" className="title">
             Watch Buddy
           </Typography>
           {props.auth && (
@@ -68,6 +56,7 @@ export default function MenuAppBar(props) {
               >
                 <MenuItem onClick={handleClose}>Friend Requests</MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
               </Menu>
             </div>
           )}
