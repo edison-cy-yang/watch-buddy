48535340e97146a174ded9077555ca6fd771aa28
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-23 09:53:26 -0700

    added proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     '/users',
     createProxyMiddleware({
       target: 'http://localhost:8080',
-      changeOrigin: true,
+      changeOrigin: false,
     })
   );
 };
