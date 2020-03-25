93787bc0ff1eb074646adf4d6821daba184e0e11
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-24 17:23:55 -0700

    very basic nav

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Feed(props) {
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <h1>I am feed</h1>
   );
