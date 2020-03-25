947904c36728114866b1763d3e848c0e3c2bbd9f
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-25 10:53:02 -0700

    transformed rooms data into grouped by owner id

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 
 import UserContext from '../contexts/UserContext';
 
@@ -7,17 +7,34 @@ import axios from 'axios';
 export default function Feed(props) {
   //get current user's id
   const auth = useContext(UserContext);
-  console.log(auth);
+  const [rooms, setRooms] = useState({});
+
+  const transformRawRoomsData = (rooms) => {
+    const transformed = {};
+    rooms.forEach(room => {
+      console.log(room.owner_id);
+      if(!transformed[room.owner_id]) {
+        transformed[room.owner_id] = [room];
+      } else {
+        transformed[room.owner_id] = [...transformed[room.owner_id], room];
+      }
+    })
+    return transformed;
+  }
 
   useEffect(() => {
     if(auth.id) {
       const fetchRooms = async () => {
         const rooms = await axios.get(`/rooms?id=${auth.id}`);
         console.log(rooms.data);
+        const transformedRooms = transformRawRoomsData(rooms.data);
+        setRooms(transformedRooms);
       }
       fetchRooms();
     }
   }, [auth])
+
+  console.log(rooms);
 
   return (
     <h1>I am feed</h1>
