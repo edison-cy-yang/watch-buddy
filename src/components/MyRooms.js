1b93f83aec62cab6194f3488486eadfc920170b0
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-26 15:39:54 -0700

    added add icon

--- src/components/MyRooms.js
+++ src/components/MyRooms.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import UserContext from '../contexts/UserContext';
+
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+
+import './MyRooms.scss';
 
 export default function MyRooms(props) {
+  const auth = useContext(UserContext);
+  console.log(auth);
   return (
-    <h1>I am My Rooms</h1>
+    <div>
+      <h1>I am My Rooms</h1>
+      <div className="add-icon">
+        <IconButton color="primary" aria-label="Create New Room">
+          <AddCircleIcon />
+        </IconButton>
+      </div>
+    </div>
   );
 };
