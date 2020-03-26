47a934d45ed844cf788b871a105a6dd031b7cb79
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-25 17:22:40 -0700

    succesfully loaded video based on video id

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,9 +1,36 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Youtube from 'react-youtube';
 
 export default function ChatRoom(props) {
 
+  const room = props.location.state;
+  console.log(room);
+
+  const getVideoId = (url) => {
+    //Get videoId
+    const rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+    const videoId = url.match(rx)[1];
+    return videoId;
+  };
+
+  const videoId = getVideoId(room.video_url);
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  };
+
   return (
-    <h1>I am inside a room</h1>
+    <div>
+      <h1>I am inside a room for <b>{room.title}</b></h1>
+      <Youtube
+        videoId={videoId}
+        opts={opts}
+      />
+    </div>
   );
 };
