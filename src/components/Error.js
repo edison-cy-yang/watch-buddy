5d234fa85e06129f6f97ab2c863efe2732c1996e
edison-cy-yang <edison.cy.yang@gmail.com>
2020-04-12 14:42:19 -0700

    styled the error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Error.scss';
+
 export default function Error(props) {
-  console.log("error");
+
   return (
-    <div>
+    <div className="error-container">
       <h2>Opps, an error happened:</h2>
       <h3>{props.error}</h3>
     </div>
