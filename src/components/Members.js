72367e8e7b84177fa7448bf4774d74d95e4bb85e
edison-cy-yang <edison.cy.yang@gmail.com>
2020-04-13 15:06:17 -0700

    number of people online increased when someone joins

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Members(props) {
-
+  const [members, setMember] = useState([]);
+  const [numOfMembers, setNumberOfMembers] = useState(0);
 
   useEffect(() => {
     console.log(props.socket);
+    props.socket.on('new member', (res) => {
+      console.log("person joined");
+      setNumberOfMembers(res.numOfPeople);
+    })
   }, [])
 
+  console.log(members);
+
   return (
-    <h3>People online: </h3>
+    <div>
+      <h3>People online: {numOfMembers}</h3>
+    </div>
   );
 };
